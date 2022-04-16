2e2d6802340707e3c5658d2a2fc2977f8258517b
Augustine Tabeling <awdtab@ymail.com>
2022-04-15 23:21:19 -0400

    define basic hard config & write to settings.json

--- src/main.js
+++ src/main.js
@@ -23,5 +23,11 @@ if (!fs.existsSync(envpaths.config)) {
 let configfilename = envpaths.config+'/settings.json'
 // if settings.json DOES NOT exist(!) then create it and wait (sync) until its created.
 if (!fs.existsSync(configfilename)){
-    fs.writeFileSync(configfilename, '{}')
+    //define basic hardcoded config
+    let baseconfig = {
+        'projects-root':'c:/projects',
+        'dropbox-token': "dfgrlgrfhgkkfjgb"
+    }
+    //stringify the json "baseconfig" object and write it syncroniously (wait until it is created to proceed)
+    fs.writeFileSync(configfilename, JSON.stringify(baseconfig))
 }
