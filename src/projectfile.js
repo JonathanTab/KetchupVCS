cbc3a8bc5e94d674e67e13d42676b7e88dd974c3
Augustine Tabeling <awdtab@ymail.com>
2022-04-16 04:36:32 -0400

    Fix projectfile class extension

--- src/projectfile.js
+++ src/projectfile.js
@@ -2,27 +2,40 @@
 //SCHEMA
 //{ "revisions" : [ {'timestamp': '123123', 'author': 'shop-pc', 'changed': ['path': 'demo-brace.skp', 'changetype': 'saved', 'thumbnail': 'base64']},... ], "projectname": "something"  }
 //
-
+import fs from 'fs';
 
 export default class ProjectFile {
     revisions = []
     projectname = ""
     constructor(path) {
+        this.path = path
         //path is literal path to the project file
         //if not exist, fill out schema with skeleton project file, then write to file
         //else, load file into schema
 
-        this.save()
+        if (!fs.existsSync(path)){
+            this.revisions = []
+            this.projectname = "ourproject"
+            this.save()
 
-        this.load()
+        }
+        else{
+            this.load()
+        }
+ 
+
+
 
     }
 
     async save() {
+        fs.writeFileSync(this.path, JSON.stringify(this.revisions + this.projectname))
 
     }
     async load() {
-
+        
+        theparseddata = JSON.parse(fs.readFileSync(this.path))
+        console.log(theparseddata)
     }
     appendRevision(revision) {
         this.save()
