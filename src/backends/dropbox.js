59dba180a36dd5ac09dc4757b28875ab0f4f84a5
Augustine Tabeling <awdtab@ymail.com>
2022-04-19 01:25:14 -0400

    implemented dropbox backend upload function

--- src/backends/dropbox.js
+++ src/backends/dropbox.js
@@ -8,6 +8,7 @@ import { Dropbox } from 'dropbox';
 // Our code
 import log from '../log.js';
 import * as util from '../util.js';
+import { readFile } from 'fs';
 
 // We need:
 // downloadFile(url, path), downloads file from dropbox root folder
@@ -25,6 +26,15 @@ export default class DropboxBackend {
     return (this.config.data.dropboxBackend.token && this.config.data.dropboxBackend.rootFolder);
   }
 
+  init(){
+    if (this.isConfigured() == true){
+      this.dbx = new Dropbox.Dropbox({ accessToken: this.config.data.dropboxBackend.token});
+      return true
+    } else {
+      return false
+    }
+  }
+
   downloadFile(url, filepath) {
     return new Promise((resolve, reject) => {
 
@@ -39,7 +49,17 @@ export default class DropboxBackend {
 
   storeFile(url, filepath) {
     return new Promise((resolve, reject) => {
-
+      file = fs.readFile (filepath, (err,data )){}
+      readFile(filepath).then(fileBuffer => {
+        dbx.filesUpload({path: this.config.data.dropboxBackend.rootFolder+"/"+url, contents: fileBuffer})
+        .then(function(response) {
+        })
+        .catch(function(error) {
+          console.error(error);
+        });
+      }).catch(error => {
+        console.error(error.message);
+        process.exit(1);
+      });
     });
   }
-}
